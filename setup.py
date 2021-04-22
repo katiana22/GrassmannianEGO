c432f0c26ee9a70d5f680830259ba1cbc5cb22f4
Katiana Kontolati <kkontol1@jhu.edu>
2021-04-22 16:32:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,6 @@ def check_python_version():
         sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
 
 
-def read_package_variable(key):
-    """Read the value of a variable from the package without importing."""
-    module_path = os.path.join(PACKAGE_NAME, '__init__.py')
-    with open(module_path) as module:
-        for line in module:
-            parts = line.strip().split(' ')
-            if parts and parts[0] == key:
-                return parts[-1].strip("'")
-    assert 0, "'{0}' not found in '{1}'".format(key, module_path)
-
-
 check_python_version()
 setup(
     name='GrassmannianEGO',
